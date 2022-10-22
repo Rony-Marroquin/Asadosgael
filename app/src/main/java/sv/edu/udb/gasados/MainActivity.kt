63f31e2b6649c957a2494786fa530c8a9abd626e
Rony.Marroquin <ronymarroquin4991@gmail.com>
2022-10-21 21:16:20 -0600

    segunda version

--- app/src/main/java/sv/edu/udb/gasados/MainActivity.kt
+++ app/src/main/java/sv/edu/udb/gasados/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.navigation.ui.setupWithNavController
 import sv.edu.udb.gasados.databinding.ActivityMainBinding
 import java.util.Objects
 import java.util.TimerTask
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import sv.edu.udb.gasados.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+           //startActivity()
+
+
+       val edt1 =findViewById<EditText>(R.id.edt1)
+        val edt2 =findViewById<EditText>(R.id.edt2)
+        val boton1=findViewById<Button>(R.id.agregar1)
+        val boton2=findViewById<Button>(R.id.agregar2)
+
 
 
 
@@ -38,8 +51,27 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 
+  /* fun starTimer(){
+        object : CountDownTimer(2000,1000) {
+            override fun onTick(p0: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFinish() {
+                TODO("Not yet implemented")
+
+                val intent= Intent(applicationContext,ActivityMainBinding::class.java).apply {  }
+                startActivity(intent)}
+                }
+
+
+
+        }
+
+*/
 
 
 
